bc522e30a4f4d7952df75ce883968ba8b87d70c1
Leo <cli4168@hillstonenet.com>
2024-02-23 17:51:26 +0800

    Update cmmand specification with let syntax
    update dune file for minimum dependencies

--- lib/cmdparser.ml
+++ lib/cmdparser.ml
@@ -1,12 +1,17 @@
-open Core
-
-let do_hash file = Md5.digest_file_blocking file |> Md5.to_hex |> print_endline
+let do_hash hash_length file =
+  let open Core in
+  Md5.digest_file_blocking file
+  |> Md5.to_hex
+  |> (fun s -> String.prefix s hash_length)
+  |> print_endline
 
 let command =
   Command.basic
     ~summary:"Generate an MD5 hash of the input data"
     ~readme:(fun () -> "More details information")
-    Command.Param.(
-      map
-        (anon ("filename" %: string))
-        ~f:(fun filename () -> do_hash filename))
+    (let open Command.Let_syntax in
+     let open Command.Param in
+     let%map hash_length = anon ("hash_length" %: int)
+     and filename = anon ("filename" %: string) in
+     fun () -> do_hash hash_length filename)
+
